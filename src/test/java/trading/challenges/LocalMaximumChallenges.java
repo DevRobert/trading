fa549bb13ffff97f755d50815e33e5786dbf2750
Robert Blutner <robert@blutner.de>
2018-03-14 17:21:52 +0100

    Parameters for local maximum challenges adjusted

--- src/test/java/trading/challenges/LocalMaximumChallenges.java
+++ src/test/java/trading/challenges/LocalMaximumChallenges.java
@@ -18,18 +18,9 @@ public class LocalMaximumChallenges extends ProgressiveChallengesBase {
     private static List<Integer> getLocalMaximumLookBehindPeriods() {
         List<Integer> result = new ArrayList<>();
 
-        result.add(1);
-        result.add(2);
-        result.add(5);
-        result.add(10);
-        result.add(20);
-        result.add(30);
-        result.add(40);
-        result.add(50);
-        result.add(60);
-        result.add(70);
-        result.add(80);
-        result.add(90);
+        for(int i = 1; i <= 120; i++) {
+            result.add(i);
+        }
 
         return result;
     }
@@ -37,6 +28,33 @@ public class LocalMaximumChallenges extends ProgressiveChallengesBase {
     private static List<Double> getMinDistanceFromLocalMaximumPercentage() {
         List<Double> result = new  ArrayList<>();
 
+        result.add(0.0001);
+        result.add(0.00011);
+        result.add(0.00012);
+        result.add(0.00013);
+        result.add(0.00014);
+        result.add(0.00015);
+        result.add(0.00016);
+        result.add(0.00017);
+        result.add(0.00018);
+        result.add(0.00019);
+        result.add(0.0002);
+        result.add(0.00021);
+        result.add(0.00022);
+        result.add(0.00023);
+        result.add(0.00024);
+        result.add(0.00025);
+        result.add(0.00026);
+        result.add(0.00027);
+        result.add(0.00028);
+        result.add(0.00029);
+        result.add(0.0003);
+        result.add(0.0004);
+        result.add(0.0005);
+        result.add(0.0006);
+        result.add(0.0007);
+        result.add(0.0008);
+        result.add(0.0009);
         result.add(0.001);
         result.add(0.002);
         result.add(0.003);
@@ -47,21 +65,38 @@ public class LocalMaximumChallenges extends ProgressiveChallengesBase {
         result.add(0.008);
         result.add(0.009);
         result.add(0.01);
+        result.add(0.015);
         result.add(0.02);
+        result.add(0.025);
         result.add(0.03);
+        result.add(0.035);
         result.add(0.04);
+        result.add(0.045);
         result.add(0.05);
+        result.add(0.055);
         result.add(0.06);
+        result.add(0.065);
         result.add(0.07);
+        result.add(0.075);
         result.add(0.08);
+        result.add(0.085);
         result.add(0.09);
+        result.add(0.095);
         result.add(0.1);
         result.add(0.11);
         result.add(0.12);
         result.add(0.13);
         result.add(0.14);
         result.add(0.15);
-        result.add(0.2);
+        result.add(0.16);
+        result.add(0.17);
+        result.add(0.18);
+        result.add(0.19);
+        result.add(0.20);
+        result.add(0.21);
+        result.add(0.22);
+        result.add(0.23);
+        result.add(0.24);
         result.add(0.25);
 
         return result;
