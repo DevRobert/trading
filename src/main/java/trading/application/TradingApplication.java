a8f0e343645940179bb809e906d2229ed279e3c5
Robert Blutner <robert@blutner.de>
2018-03-14 19:38:23 +0100

    Report simulation duration

--- src/main/java/trading/application/TradingApplication.java
+++ src/main/java/trading/application/TradingApplication.java
@@ -4,13 +4,24 @@ import trading.challenges.Challenge;
 import trading.challenges.ChallengeExecutor;
 import trading.challenges.LocalMaximumChallenge;
 
+import java.util.Date;
+
 public class TradingApplication {
     public static void main(String[] args) {
         System.out.println("Trading application started.");
 
         Challenge challenge = new LocalMaximumChallenge();
 
+        Date start = new Date();
+
         ChallengeExecutor challengeExecutor = new ChallengeExecutor();
         challengeExecutor.executeChallenge(challenge);
+
+        Date end = new Date();
+
+        long durationMilliseconds = end.getTime() - start.getTime();
+        double durationSeconds = ((double) durationMilliseconds) / 1000.0;
+
+        System.out.println("Simulation duration: " + durationSeconds + " seconds");
     }
 }
