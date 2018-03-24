2d9ed5d2b367d7150b39d8e46ca54575fe42bbaf
Robert Blutner <robert@blutner.de>
2018-03-24 15:34:28 +0100

    Local maximum trading strategy refactored

--- src/main/java/trading/strategy/localMaximum/LocalMaximumTradingStrategy.java
+++ src/main/java/trading/strategy/localMaximum/LocalMaximumTradingStrategy.java
@@ -8,8 +8,6 @@ import trading.strategy.TradingStrategyContext;
 import trading.strategy.progressive.ProgressiveTradingStrategy;
 import trading.strategy.progressive.ProgressiveTradingStrategyParametersBuilder;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 /**
  * Trading strategy:
  *
@@ -18,65 +16,64 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class LocalMaximumTradingStrategy implements TradingStrategy {
     private final ProgressiveTradingStrategy progressiveTradingStrategy;
+    private final HistoricalStockData historicalStockData;
+    private final LocalMaximumTradingStrategyParameters parameters;
+
+    private double buyLocalMaximum = 0.0;
+    private boolean buyLocalMaximumPassed = false;
+    private double maximumSinceBuying = 0.0;
 
     public LocalMaximumTradingStrategy(LocalMaximumTradingStrategyParameters parameters, TradingStrategyContext context) {
+        this.parameters = parameters;
+        this.historicalStockData = context.getHistoricalMarketData().getStockData(parameters.getIsin());
+
         ProgressiveTradingStrategyParametersBuilder parametersBuilder = new ProgressiveTradingStrategyParametersBuilder();
 
         parametersBuilder.setISIN(parameters.getIsin());
 
-        AtomicReference<Double> buyLocalMaximum = new AtomicReference<>(0.0);
-        AtomicReference<Boolean> buyLocalMaximumPassed = new AtomicReference<>(false);
-        AtomicReference<Double> maximumSinceBuying = new AtomicReference<>(0.0);
-
-        parametersBuilder.setBuyTriggerFactory(historicalMarketData -> {
-            HistoricalStockData historicalStockData = historicalMarketData.getStockData(parameters.getIsin());
-
-            return new DelegateTrigger(() -> {
-                double localMaximum = historicalStockData.getMaximumClosingMarketPrice(parameters.getBuyTriggerLocalMaximumLookBehindPeriod()).getValue();
-                double minDelta = localMaximum * parameters.getBuyTriggerMinDistanceFromLocalMaximumPercentage();
-                double maxBuyPrice = localMaximum - minDelta;
-                double lastClosingMarketPrice = historicalStockData.getLastClosingMarketPrice().getValue();
-
-                if(lastClosingMarketPrice <= maxBuyPrice) {
-                    buyLocalMaximum.set(localMaximum);
-                    buyLocalMaximumPassed.set(false);
-                    maximumSinceBuying.set(0.0);
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            });
-        });
-
-        parametersBuilder.setSellTriggerFactory(historicalMarketData -> {
-            HistoricalStockData historicalStockData = historicalMarketData.getStockData(parameters.getIsin());
-
-            return new DelegateTrigger(() -> {
-                double lastClosingPrice = historicalStockData.getLastClosingMarketPrice().getValue();
-
-                if(!buyLocalMaximumPassed.get()) {
-                    buyLocalMaximumPassed.set(lastClosingPrice >= buyLocalMaximum.get());
-                }
-
-                if(!buyLocalMaximumPassed.get()) {
-                    return false;
-                }
-
-                if(lastClosingPrice > maximumSinceBuying.get()) {
-                    maximumSinceBuying.set(lastClosingPrice);
-                }
-
-                double sellTriggerMinDeltaFromMaximumSinceBuying = parameters.getSellTriggerMinDistanceFromMaximumSinceBuyingPercentage() * maximumSinceBuying.get();
-                double sellTriggerMaxPrice = maximumSinceBuying.get() - sellTriggerMinDeltaFromMaximumSinceBuying;
-
-                return lastClosingPrice <= sellTriggerMaxPrice;
-            });
-        });
-
+        parametersBuilder.setBuyTriggerFactory(historicalMarketData -> new DelegateTrigger(() -> this.shouldBuyStocks()));
+        parametersBuilder.setSellTriggerFactory(historicalMarketData -> new DelegateTrigger(() -> this.shouldSellStocks()));
         parametersBuilder.setResetTriggerFactory(historicalMarketData -> new AlwaysFiresTrigger());
 
         this.progressiveTradingStrategy = new ProgressiveTradingStrategy(parametersBuilder.build(), context);
+    }
+
+    private boolean shouldBuyStocks() {
+        double localMaximum = this.historicalStockData.getMaximumClosingMarketPrice(this.parameters.getBuyTriggerLocalMaximumLookBehindPeriod()).getValue();
+        double minDelta = localMaximum * this.parameters.getBuyTriggerMinDistanceFromLocalMaximumPercentage();
+        double maxBuyPrice = localMaximum - minDelta;
+        double lastClosingMarketPrice = this.historicalStockData.getLastClosingMarketPrice().getValue();
+
+        if(lastClosingMarketPrice <= maxBuyPrice) {
+            this.buyLocalMaximum = localMaximum;
+            this.buyLocalMaximumPassed = false;
+            this.maximumSinceBuying = 0.0;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean shouldSellStocks() {
+        double lastClosingPrice = this.historicalStockData.getLastClosingMarketPrice().getValue();
+
+        if(!this.buyLocalMaximumPassed) {
+            this.buyLocalMaximumPassed = lastClosingPrice >= this.buyLocalMaximum;
+        }
+
+        if(!this.buyLocalMaximumPassed) {
+            return false;
+        }
+
+        if(lastClosingPrice > this.maximumSinceBuying) {
+            this.maximumSinceBuying = lastClosingPrice;
+        }
+
+        double sellTriggerMinDeltaFromMaximumSinceBuying = this.parameters.getSellTriggerMinDistanceFromMaximumSinceBuyingPercentage() * this.maximumSinceBuying;
+        double sellTriggerMaxPrice = this.maximumSinceBuying - sellTriggerMinDeltaFromMaximumSinceBuying;
+
+        return lastClosingPrice <= sellTriggerMaxPrice;
     }
 
     @Override
