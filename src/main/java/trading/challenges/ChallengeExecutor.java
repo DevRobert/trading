53772074fa5f21b99d577642e90078e57d9936a2
Robert Blutner <robert@blutner.de>
2018-04-03 21:43:02 +0200

    Refactoring of report generation

--- src/main/java/trading/challenges/ChallengeExecutor.java
+++ src/main/java/trading/challenges/ChallengeExecutor.java
@@ -13,32 +13,19 @@ import java.util.concurrent.Executors;
 public class ChallengeExecutor {
     private ParameterTupleSource runParametersSource;
     private Iterator<Object[]> runParametersIterator;
-    private Reporter reporter;
-    private long nextRunIndex = 0;
+    private ChallengeReporter reporter;
+    private int nextRunIndex = 0;
+    private CountDownLatch countDownLatch;
+    private int numSimulations;
+    private Challenge challenge;
 
     private void prepareReporting(String[] parameterNames) {
         String fileName = "/Users/robert/GitHub/data/data.csv";
-
-        this.reporter = new Reporter(fileName);
-
-        List<String> fieldNames = new ArrayList();
-        fieldNames.add("simulation");
-        fieldNames.add("initial_account_balance");
-        fieldNames.add("final_account_balance");
-        fieldNames.add("average_market_rate_of_return");
-        fieldNames.add("realized_rate_of_return");
-        fieldNames.add("added_rate_of_return");
-        fieldNames.add("transactions");
-
-        for(String parameterName: parameterNames) {
-            fieldNames.add(parameterName);
-        }
-
-        this.reporter.writeLine(-1, String.join(";", fieldNames));
+        this.reporter = new ChallengeReporter(fileName, parameterNames);
     }
 
     private void endReporting() {
-       this.reporter.finish();
+       this.reporter.close();
        this.reporter = null;
     }
 
@@ -55,10 +42,15 @@ public class ChallengeExecutor {
             }
         }
 
+        if(runParameters == null) {
+            return null;
+        }
+
         return new RunParameters(runIndex, runParameters);
     }
 
     public void executeChallenge(Challenge challenge) {
+        this.challenge = challenge;
         this.prepareReporting(challenge.getParameterNames());
 
         this.runParametersSource = challenge.getParametersSource();
@@ -66,41 +58,16 @@ public class ChallengeExecutor {
 
         final int numThreads = Runtime.getRuntime().availableProcessors();
 
-        int numSimulations = runParametersSource.size();
-        System.out.println("Running " + String.format("%,d", numSimulations) + " simulations in " + numThreads + " threads...");
+        this.numSimulations = runParametersSource.size();
+        System.out.println("Running " + String.format("%,d", this.numSimulations) + " simulations in " + numThreads + " threads...");
 
         final ExecutorService threads = Executors.newFixedThreadPool(numThreads);
 
+        this.countDownLatch = new CountDownLatch(this.numSimulations);
+
         try {
-            final CountDownLatch countDownLatch = new CountDownLatch(runParametersSource.size());
-
             for(int threadIndex = 0; threadIndex < numThreads; threadIndex++) {
-                threads.execute(() -> {
-                    RunParameters runParameters;
-
-                    while((runParameters = this.getNextRunParameters()) != null) {
-                        try {
-                            SimulationDriverParameters simulationDriverParameters = challenge.buildSimulationDriverParametersForRun(runParameters.getParameters());
-                            SimulationDriver simulationDriver = new SimulationDriver(simulationDriverParameters);
-                            SimulationReport simulationReport = simulationDriver.runSimulation();
-
-                            this.trackSimulationReport(simulationReport, runParameters);
-                        }
-                        catch(RuntimeException ex) {
-                            this.reportException(runParameters, ex);
-                        }
-                        finally {
-                            countDownLatch.countDown();
-                        }
-
-                        long numRemainingSimulations = countDownLatch.getCount();
-
-                        if(numRemainingSimulations % 100000 == 0 && numRemainingSimulations > 0) {
-                            double progress = 100.0 * (numSimulations - numRemainingSimulations) / numSimulations;
-                            System.out.println("Progress: " + String.format("%.2f", progress) + " % - " + String.format("%,d", numRemainingSimulations) + " simulations remaining.");
-                        }
-                    }
-                });
+                threads.execute(() -> this.runSimulationInWorkerThread());
             }
 
             countDownLatch.await();
@@ -117,34 +84,30 @@ public class ChallengeExecutor {
         this.endReporting();
     }
 
-    private void trackSimulationReport(SimulationReport simulationReport, RunParameters runParameters) {
-        String line = runParameters.getRunIndex() +
-                ";" + simulationReport.getInitialAccountBalance().toString() +
-                ";" + simulationReport.getFinalAccountBalance().toString() +
-                ";" + simulationReport.getAverageMarketRateOfReturn() +
-                ";" + simulationReport.getRealizedRateOfReturn() +
-                ";" + simulationReport.getAddedRateOfReturn() +
-                ";" + simulationReport.getTransactions().size();
+    private void runSimulationInWorkerThread() {
+        RunParameters runParameters;
 
-        for(Object runParameter: runParameters.getParameters()) {
-            line += ";" + runParameter.toString();
+        while((runParameters = this.getNextRunParameters()) != null) {
+            try {
+                SimulationDriverParameters simulationDriverParameters = challenge.buildSimulationDriverParametersForRun(runParameters.getParameters());
+                SimulationDriver simulationDriver = new SimulationDriver(simulationDriverParameters);
+                SimulationReport simulationReport = simulationDriver.runSimulation();
+
+                this.reporter.trackCompletedSimulation(runParameters, simulationReport);
+            }
+            catch(RuntimeException ex) {
+                this.reporter.trackFailedSimulation(runParameters, ex);
+            }
+            finally {
+                countDownLatch.countDown();
+            }
+
+            long numRemainingSimulations = countDownLatch.getCount();
+
+            if(numRemainingSimulations % 100000 == 0 && numRemainingSimulations > 0) {
+                double progress = 100.0 * (numSimulations - numRemainingSimulations) / numSimulations;
+                System.out.println("Progress: " + String.format("%.2f", progress) + " % - " + String.format("%,d", numRemainingSimulations) + " simulations remaining.");
+            }
         }
-
-        this.reporter.writeLine(runParameters.getRunIndex(), line);
-    }
-
-    private void reportException(RunParameters runParameters, Exception ex) {
-        String message = "Exception occurred for run index " + runParameters.getRunIndex();
-
-        for(int parameterIndex = 0; parameterIndex < runParameters.getParameters().length; parameterIndex++) {
-            message += ";";
-            message += runParameters.getParameters()[parameterIndex].toString();
-        }
-
-        message += ";" + ex.getMessage();
-
-        System.out.println(message);
-
-        this.reporter.skipLine(runParameters.getRunIndex());
     }
 }
