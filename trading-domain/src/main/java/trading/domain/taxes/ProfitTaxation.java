fcb8bd6f72c8ba24b393a8e1be869d61f6155f90
DevRobert <robert@blutner.de>
2020-02-14 19:01:55 +0100

    Profit Taxation added

--- trading-domain/src/main/java/trading/domain/taxes/ProfitTaxation.java
+++ trading-domain/src/main/java/trading/domain/taxes/ProfitTaxation.java
@@ -1,6 +1,7 @@
 package trading.domain.taxes;
 
 import trading.domain.Amount;
+import trading.domain.DomainException;
 
 public class ProfitTaxation {
     private final TaxCalculator taxCalculator;
@@ -8,7 +9,7 @@ public class ProfitTaxation {
     private Amount taxableProfit = Amount.Zero;
     private Amount taxedProfit = Amount.Zero;
     private Amount reservedTaxes = Amount.Zero;
-    private Amount confirmedTaxes = Amount.Zero;
+    private Amount paidTaxes = Amount.Zero;
 
     public ProfitTaxation(TaxCalculator taxCalculator) {
         this.taxCalculator = taxCalculator;
@@ -26,11 +27,32 @@ public class ProfitTaxation {
         return reservedTaxes;
     }
 
-    public Amount getConfirmedTaxes() {
-        return confirmedTaxes;
+    public Amount getPaidTaxes() {
+        return paidTaxes;
     }
 
-    public void registerProfit(Profit profit) {
+    public void registerProfit(Amount profit) {
+        this.taxableProfit = this.taxableProfit.add(profit);
+        this.recalculateReservedTaxes();
+    }
 
+    public void registerTaxPayment(Amount taxedProfit, Amount paidTaxes) {
+        if(taxedProfit.getValue() > this.taxableProfit.getValue()) {
+            throw new DomainException("The specified taxed profit must not exceed the accrued taxable profit.");
+        }
+
+        this.taxableProfit = this.taxableProfit.subtract(taxedProfit);
+        this.taxedProfit = this.taxedProfit.add(taxedProfit);
+        this.paidTaxes = this.paidTaxes.add(paidTaxes);
+        this.recalculateReservedTaxes();
+    }
+
+    private void recalculateReservedTaxes() {
+        if(this.taxableProfit.getValue() >= 0.0) {
+            this.reservedTaxes = this.taxCalculator.calculateTaxes(this.taxableProfit);
+        }
+        else {
+            this.reservedTaxes = Amount.Zero;
+        }
     }
 }
--- trading-domain/src/test/java/trading/domain/taxes/ProfitTaxationTest.java
+++ trading-domain/src/test/java/trading/domain/taxes/ProfitTaxationTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import trading.domain.Amount;
+import trading.domain.DomainException;
 
 /**
  * For various scenarios key figures are tested.
@@ -11,9 +12,9 @@ import trading.domain.Amount;
  * Key Figures:
  *
  * - TaxableProfit: The profit to be taxed
- * - TaxedProfit: The profit that has been taxed (taxes have been confirmed)
+ * - TaxedProfit: The profit that has been taxed (taxes have been paid)
  * - ReservedTaxes: The amount of money to be reserved for taxable profit
- * - ConfirmedTaxes: The amount of money that has been confirmed for paying taxes of taxed profit
+ * - PaidTaxes: The amount of taxes that have been paid
  *
  * Scenarios:
  *
@@ -21,7 +22,7 @@ import trading.domain.Amount;
  * 2) Simple Loss (loss should be captured as negative taxable profit, no tax reservation)
  * 3) Profit after Loss (taxable profit should increase, tax reservation should only be established for amount over loss compensation)
  * 4) Loss after Profit (loss should be subtracted from taxable profit, tax reservation should be decreased)
- * 5) Tax Confirmation (taxable profit should shift to taxed profit, reserved taxes should shift to confirmed taxes)
+ * 5) Tax Payment (taxable profit should shift to taxed profit, reserved taxes should shift to paid taxes)
  * 6) Loss after Taxed Profit (loss should be captured as negative taxable profit, no tax reservation)
  */
 public class ProfitTaxationTest {
@@ -36,14 +37,14 @@ public class ProfitTaxationTest {
     // Simple Profit
 
     private void registerSimpleProfit() {
-        Profit profit = new Profit(ProfitCategories.Sale, new Amount(1000.0));
+        Amount profit = new Amount(1000.0);
         this.profitTaxation.registerProfit(profit);
     }
 
     @Test
     public void profitIncreasesTaxableProfit() {
         this.registerSimpleProfit();
-        Assert.assertEquals(new Amount(250.0), this.profitTaxation.getTaxableProfit());
+        Assert.assertEquals(new Amount(1000.0), this.profitTaxation.getTaxableProfit());
     }
 
     @Test
@@ -59,16 +60,16 @@ public class ProfitTaxationTest {
     }
 
     @Test
-    public void profitDoesNotAffectConfirmedTaxes() {
+    public void profitDoesNotAffectPaidTaxes() {
         this.registerSimpleProfit();
-        Assert.assertEquals(Amount.Zero, this.profitTaxation.getConfirmedTaxes());
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getPaidTaxes());
     }
 
     // Simple Loss
 
     private void registerSimpleLoss() {
-        Profit profit = new Profit(ProfitCategories.Sale, new Amount(-1000.0));
-        this.profitTaxation.registerProfit(profit);
+        Amount loss = new Amount(-1000.0);
+        this.profitTaxation.registerProfit(loss);
     }
 
     @Test
@@ -90,32 +91,203 @@ public class ProfitTaxationTest {
     }
 
     @Test
-    public void lossDoesNotAffectConfirmedTaxes() {
+    public void lossDoesNotAffectPaidTaxes() {
         this.registerSimpleLoss();
-        Assert.assertEquals(Amount.Zero, this.profitTaxation.getConfirmedTaxes());
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getPaidTaxes());
     }
 
     // Profit after Loss
 
     private void registerLossAndProfit() {
+        Amount loss = new Amount(-1000.0);
+        this.profitTaxation.registerProfit(loss);
 
+        Amount profit = new Amount(1500.0);
+        this.profitTaxation.registerProfit(profit);
+    }
+
+    @Test
+    public void profitIncreasesTaxableProfitAfterLoss() {
+        this.registerLossAndProfit();
+        Assert.assertEquals(new Amount(500.0), this.profitTaxation.getTaxableProfit());
+    }
+
+    @Test
+    public void profitAfterLossDoesNotAffectTaxedProfit() {
+        this.registerLossAndProfit();
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getTaxedProfit());
+    }
+
+    @Test
+    public void profitIncreasesReservedTaxesAfterLoss() {
+        // Total profit: 500.00
+        // Reserved Taxes 25% * 500.00 = 125.00
+
+        this.registerLossAndProfit();
+        Assert.assertEquals(new Amount(125.00), this.profitTaxation.getReservedTaxes());
+    }
+
+    @Test
+    public void profitAfterLossDoesNotAffectPaidTaxes() {
+        this.registerLossAndProfit();
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getPaidTaxes());
     }
 
     // Loss after Profit
 
     private void registerProfitAndLoss() {
+        Amount profit = new Amount(1500.0);
+        this.profitTaxation.registerProfit(profit);
 
+        Amount loss = new Amount(-2000.0);
+        this.profitTaxation.registerProfit(loss);
     }
 
-    // Tax confirmation
+    @Test
+    public void lossDecreasesTaxableProfitAfterLoss() {
+        this.registerProfitAndLoss();
+        Assert.assertEquals(new Amount(-500.0), this.profitTaxation.getTaxableProfit());
+    }
 
-    private void registerProfitAndConfirmTaxes() {
+    @Test
+    public void lossAfterProfitDoesNotAffectTaxedProfit() {
+        this.registerProfitAndLoss();
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getTaxedProfit());
+    }
 
+    @Test
+    public void lossDecreasesReservedTaxesAfterProfit() {
+        this.registerProfitAndLoss();
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getReservedTaxes());
+    }
+
+    @Test
+    public void lossAfterProfitDoesNotAffectConfirmedTaxes() {
+        this.registerProfitAndLoss();
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getPaidTaxes());
+    }
+
+    private void registerProfitAndPayTaxes() {
+        Amount profit = new Amount(1000.0);
+        this.profitTaxation.registerProfit(profit);
+
+        Amount taxedProfit = new Amount(600.0);
+        Amount paidTaxes = new Amount(150.0);
+        this.profitTaxation.registerTaxPayment(taxedProfit, paidTaxes);
+    }
+
+    @Test
+    public void taxPaymentDecreasesTaxableProfit() {
+        this.registerProfitAndPayTaxes();
+        Assert.assertEquals(new Amount(400.0), this.profitTaxation.getTaxableProfit());
+    }
+
+    @Test
+    public void taxPaymentIncreasesTaxedProfit() {
+        this.registerProfitAndPayTaxes();
+        Assert.assertEquals(new Amount(600.0), this.profitTaxation.getTaxedProfit());
+    }
+
+    @Test
+    public void taxPaymentDecreasesReservedTaxes() {
+        this.registerProfitAndPayTaxes();
+        Assert.assertEquals(new Amount(100.0), this.profitTaxation.getReservedTaxes());
+    }
+
+    @Test
+    public void taxPaymentIncreasesPaidTaxes() {
+        this.registerProfitAndPayTaxes();
+        Assert.assertEquals(new Amount(150.0), this.profitTaxation.getPaidTaxes());
     }
 
     // Loss after Taxed Profit
 
-    private void registerProfitAndConfirmTaxesAndRegisterLoss() {
+    private void registerTaxedProfitAndRegisterLoss() {
+        Amount profit = new Amount(1000.0);
+        this.profitTaxation.registerProfit(profit);
 
+        Amount taxedProfit = new Amount(1000.0);
+        Amount paidTaxes = new Amount(250.0);
+        this.profitTaxation.registerTaxPayment(taxedProfit, paidTaxes);
+
+        Amount loss = new Amount(-500.0);
+        this.profitTaxation.registerProfit(loss);
+    }
+
+    @Test
+    public void lossDecreasesTaxableProfit_independentFromAlreadyTaxedProfit() {
+        this.registerTaxedProfitAndRegisterLoss();
+        Assert.assertEquals(new Amount(-500.0), this.profitTaxation.getTaxableProfit());
+    }
+
+    @Test
+    public void lossAfterTaxedProfitDoesNotAffectTaxedProfit() {
+        this.registerTaxedProfitAndRegisterLoss();
+        Assert.assertEquals(new Amount(1000.0), this.profitTaxation.getTaxedProfit());
+    }
+
+    @Test
+    public void lossAfterTaxedProfitDoesNotAffectReservedTaxes() {
+        this.registerTaxedProfitAndRegisterLoss();
+        Assert.assertEquals(Amount.Zero, this.profitTaxation.getReservedTaxes());
+    }
+
+    @Test
+    public void lossAfterTaxedProfitDoesNotAffectPaidTaxes() {
+        this.registerTaxedProfitAndRegisterLoss();
+        Assert.assertEquals(new Amount(250.0), this.profitTaxation.getPaidTaxes());
+    }
+
+    // Tax confirmation special cases
+
+    @Test
+    public void taxPaymentFailsIfPaidTaxesExceedReservedTaxes() {
+        Amount profit = new Amount(1000.0);
+        this.profitTaxation.registerProfit(profit);
+
+        Amount taxedProfit = new Amount(1500.0);
+        Amount paidTaxes = new Amount(375.0);
+
+        try {
+            this.profitTaxation.registerTaxPayment(taxedProfit, paidTaxes);
+        }
+        catch(DomainException e) {
+            Assert.assertEquals("The specified taxed profit must not exceed the accrued taxable profit.", e.getMessage());
+            return;
+        }
+
+        Assert.fail("DomainException expected.");
+    }
+
+    @Test
+    public void taxPaymentFailsIfTaxableProfitIsNegative() {
+        Amount loss = new Amount(-1000.0);
+        this.profitTaxation.registerProfit(loss);
+
+        Amount taxedProfit = new Amount(1000.0);
+        Amount paidTaxes = new Amount(250.0);
+
+        try {
+            this.profitTaxation.registerTaxPayment(taxedProfit, paidTaxes);
+        }
+        catch(DomainException e) {
+            Assert.assertEquals("The specified taxed profit must not exceed the accrued taxable profit.", e.getMessage());
+            return;
+        }
+
+        Assert.fail("DomainException expected.");
+    }
+
+    @Test
+    public void reservedTaxesDecreaseMoreThanPaidTaxesIncrease_ifTaxRateOfTaxPaymentIsLowerThanTaxReservationDueToTaxStrategy() {
+        Amount profit = new Amount(1000.0); // Reserved taxes are 250.0 (tax rate 25%)
+        this.profitTaxation.registerProfit(profit);
+
+        Amount taxedProfit = new Amount(400.0);
+        Amount paidTaxes = new Amount(40.0); // tax rate 10%
+        this.profitTaxation.registerTaxPayment(taxedProfit, paidTaxes);
+
+        // Reserved taxes for untaxed profit 600.0 are 125.0
+        Assert.assertEquals(new Amount(150.0), this.profitTaxation.getReservedTaxes());
     }
 }
